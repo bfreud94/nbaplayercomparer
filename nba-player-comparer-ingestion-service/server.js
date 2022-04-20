312f2e4cff0d35ed2865ab71afdeb5ac49834ded
Ben Freudman <bfreud94@gmail.com>
2022-04-20 16:55:27 -0400

    refactor ingestion service

--- nba-player-comparer-ingestion-service/server.js
+++ nba-player-comparer-ingestion-service/server.js
@@ -7,47 +7,46 @@ const { getCurrentYear } = require('./util')
 require('dotenv').config()
 
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, async (err) => {
-    if (err) {
-        // eslint-disable-next-line no-console
-        console.log(err)
+    const total = await Player.collection.countDocuments()
+    console.log(total)
+    if (err || total > 0) {
+        console.log(total > 0 ? `Database already populated with ${total} records` : err)
     } else {
-        // eslint-disable-next-line no-console
         console.log(`Successfully connected to MongoDB`)
-    }
-    const year = getCurrentYear()
-    const rawPlayerData = await nba.stats.playerStats({
-        Season: year
-    })
-    const playerPromises = rawPlayerData.leagueDashPlayerStats.map(async ({
-        playerName,
-        pts,
-        reb,
-        ast,
-        stl,
-        blk
-    }) => {
-        const player = new Player({
-            name: playerName,
+
+        const year = getCurrentYear()
+        const rawPlayerData = await nba.stats.playerStats({
+            Season: year
+        })
+    
+        const playerPromises = rawPlayerData.leagueDashPlayerStats.map(async ({
+            playerName,
             pts,
             reb,
             ast,
             stl,
             blk
+        }) => {
+            const player = new Player({
+                name: playerName,
+                pts,
+                reb,
+                ast,
+                stl,
+                blk
+            })
+            try {
+                const savedPlayer = await player.save()
+                return savedPlayer
+            } catch (error) {
+                console.log(error)
+            }
+            return null
         })
-        try {
-            const savedPlayer = await player.save()
-            return savedPlayer
-        } catch (error) {
-            // eslint-disable-next-line no-console
-            console.log(error)
-        }
-        return null
-    })
-    Promise.all(playerPromises).then((data) => {
-        // eslint-disable-next-line no-console
-        console.log(`Successfully wrote ${data.length} records to the database!`)
-        // eslint-disable-next-line no-console
-        console.log(`Successfully closed MongoDB connection`)
-        mongoose.connection.close()
-    })
+        Promise.all(playerPromises).then((data) => {
+            console.log(`Successfully wrote ${data.length} records to the database!`)
+            console.log(`Successfully closed MongoDB connection`)
+            mongoose.connection.close()
+        })
+    }
 })
--- nba-player-comparer-ingestion-service/util.js
+++ nba-player-comparer-ingestion-service/util.js
@@ -1,6 +1,8 @@
 const getCurrentYear = () => {
     const today = new Date()
-    const currentYear = parseInt(today.toISOString().split('-')[0]) - 1
+    const month = today.getMonth()
+    const day = today.getDay()
+    const currentYear = (month === 9 && day > 14) || today.getFullYear() > 9 ? today.getFullYear() - 1 : today.getFullYear()
     const nextYear = (currentYear + 1).toString().substring(2, 4)
     return `${currentYear}-${nextYear}`
 }
